80f7094fd474597da3dbff81beecea7cfb8afce5
kdrtrn <dev.kadirturan@gmail.com>
2021-11-29 14:36:02 +0300

    22 Kasım Ders Sonu

---
+++ src/data/users.js
@@ -0,0 +1,57 @@
+export const users = [
+    {
+        id: 1,
+        firstName: "Engin",
+        lastName: "Demiroğ",
+        city:"Ankara",
+        age: 36,
+        creditCardNumber: "123456",
+        type: "customer"
+    },
+    {
+        id: 6,
+        firstName: "Beytullah",
+        lastName: "Zor",
+        city:"Ankara",
+        age: 36,
+        creditCardNumber: "123456",
+        type: "customer"
+    },
+    {
+        id: 2,
+        firstName: "Kadir",
+        lastName: "Turan",
+        city: "Konya",
+        age: 26,
+        creditCardNumber: "654321",
+        type: "customer"
+    },
+    {
+        id: 3,
+        firstName: "Cansu",
+        lastName: "Değirmen",
+        city: "İstanbul",
+        salary: 8000,
+        age: 20,
+        type: "employee"
+    },
+    {
+        id: 4,
+        firstName: "Faik",
+        lastName: "Düzce",
+        city: "Bolu",
+        age: 23,
+        salary:1700,
+        type: "employee"
+    },
+    {
+        id: 5,
+        firstName: "Eren",
+        lastName: "Arı",
+        city: "Diyarbakır",
+        age: 20,
+        salary: 1500,
+        type: "employee"
+    }
+
+]
